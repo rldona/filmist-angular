939effe8d1ab0f21f54157f9027c62c16cdc6087
Raúl López Doña <rldona@gmail.com>
2021-06-09 15:31:22 +0200

    [FEAT] top-list migrated

---
+++ src/app/actor-detail/actor-detail.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { DbmoviesService } from '../dbmovies.service';
+import { ActivatedRoute } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'fm-actor-detail',
+  templateUrl: './actor-detail.component.html',
+  styleUrls: ['./actor-detail.component.scss']
+})
+export class ActorDetailComponent implements OnInit {
+  actorId: any;
+  actorDetail: any;
+  movieList: any;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _dbmoviesService: DbmoviesService,
+    private translate: TranslateService
+  ) { }
+
+  ngOnInit() {
+    let actorDetailResolve = this._route.snapshot.data['actor'];
+
+    this._route.params.subscribe((params: any) => {
+      this.actorId = +params['id'];
+    });
+
+    // API
+    this.actorDetail = actorDetailResolve[0];
+    this.movieList = actorDetailResolve[1];
+
+    this.translate.onLangChange.subscribe(() => {
+      this.reloadData();
+    });
+  }
+
+  reloadData() {
+    this._dbmoviesService.getMoviesByActor(this.actorId).then((res: any) => {
+      this.movieList = res;
+    });
+  }
+}
---
+++ src/app/top-list/top-list.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { DbmoviesService } from '../dbmovies.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'fm-top-list',
+  templateUrl: './top-list.component.html',
+  styleUrls: ['./top-list.component.scss']
+})
+export class TopListComponent implements OnInit {
+  collection: string;
+  topList: any;
+
+  lists: any;
+
+  constructor(private _router: ActivatedRoute,
+              private _dbmoviesService: DbmoviesService,
+              private _route: ActivatedRoute) { }
+
+  ngOnInit() {
+    let indexCollection = 0;
+
+    this._router.params.subscribe(params => {
+      this.collection = params['collection'];
+    });
+
+    let type: any = this._dbmoviesService.getCurrentType();
+
+    switch (this.collection) {
+      case 'now-playing':
+        indexCollection = 0;
+        break;
+      case 'upcoming':
+        indexCollection = 2;
+        break;
+      case 'top-rated':
+        indexCollection = type === 'tv' ? 5 : 3;
+        break;
+      case 'popular':
+        indexCollection = type === 'tv' ? 6 : 1;
+        break;
+      case 'on-the-air':
+        indexCollection = 4;
+        break;
+      default:
+        indexCollection = 0;
+        break;
+    }
+
+    this.lists = this._route.snapshot.data['home'][indexCollection];
+
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+  }
+}
